9e3dc32044d807757f7761d691a01f50737aa4d2
kmlla <93437435+kmlla@users.noreply.github.com>
2024-10-25 21:23:01 +0300

    Add files via upload

---
+++ laboratory_work_2/task_1.py
@@ -0,0 +1,17 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+months = 0
+
+while money_capital > 0:
+    money_capital += (salary - spend)
+    if money_capital < 0:
+        break
+    spend *= 1 + increase
+    months += 1
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
